a089dc82e100518eac8e31f9ab95c466c3b6ab65
gaolingfeng <491550934@qq.com>
2020-11-28 20:16:49 +0800

    注册中心服务端

---
+++ eureka-server/src/main/java/com/springcloudbase/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.springcloudbase.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author gaolingfeng
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
